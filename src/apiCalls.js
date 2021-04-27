aef044acf03234af6f09e4cc9081df155a628060
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-27 12:57:48 -0400

    Add Post order api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,16 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrder = (body) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+  ).then(res => res.json())
+  .catch(err => console.log(err))
+
+}
