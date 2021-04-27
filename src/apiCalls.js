a91bdacd37dca7486f903bdfbdec5616359544ee
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-27 13:37:34 -0400

    Add removeOrder APi call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,5 +13,12 @@ export const postOrder = (body) => {
   }
   ).then(res => res.json())
   .catch(err => console.log(err))
+}
 
+export const removeOrder = (id) => {
+  return fetch('http://localhost:3001/api/v1/orders/' + String(id) + '/', {
+    method: 'DELETE'
+  }
+  ).then(res => res.json())
+  .catch(err => console.log(err))
 }
